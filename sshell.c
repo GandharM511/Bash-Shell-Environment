89189e226caea8e5906211875c2653bac5f0f1e6
GandharM511 <gandhar.mannur@gmail.com>
2023-01-15 16:05:00 -0800

    Initial Commit

---
+++ sshell.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define CMDLINE_MAX 512
+
+int main(void)
+{
+        char cmd[CMDLINE_MAX];
+
+        while (1) {
+                char *nl;
+                int retval;
+
+                /* Print prompt */
+                printf("sshell$ ");
+                fflush(stdout);
+
+                /* Get command line */
+                fgets(cmd, CMDLINE_MAX, stdin);
+
+                /* Print command line if stdin is not provided by terminal */
+                if (!isatty(STDIN_FILENO)) {
+                        printf("%s", cmd);
+                        fflush(stdout);
+                }
+
+                /* Remove trailing newline from command line */
+                nl = strchr(cmd, '\n');
+                if (nl)
+                        *nl = '\0';
+
+                /* Builtin command */
+                if (!strcmp(cmd, "exit")) {
+                        fprintf(stderr, "Bye...\n");
+                        break;
+                }
+
+                /* Regular command */
+                retval = system(cmd);
+                fprintf(stdout, "Return status value for '%s': %d\n",
+                        cmd, retval);
+        }
+
+        return EXIT_SUCCESS;
+}
